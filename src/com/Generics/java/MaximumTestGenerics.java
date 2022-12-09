a9466edc76543b61c0e69abb8a381b7ad618ccfd
kapilmahajan123 <montyempire123gmail.com>
2022-12-09 11:37:56 +0530

    Added: Refactoring all three methods(Integer, Float and String)

--- src/com/Generics/java/MaximumTestGenerics.java
+++ src/com/Generics/java/MaximumTestGenerics.java
@@ -5,28 +5,32 @@ package com.Generics.java;
  * @author Kapil
  *
  */
-public class MaximumTestGenerics {
+public class MaximumTestGenerics<T>{
+	T num1, num2, num3;
 	
 	/*
-	 * Given three String values
+	 * Here i have used Generic functions.
+	 * T is the type of data.
 	 * then used compare to method 
+	 * 
 	 */
-	public static String maximumString(String num1, String num2, String num3) {
+	public static <T extends Comparable<T>> T maximumAmongThree(T num1, T num2, T num3) {
 
-		String maximumString = num1;
+		T maximum = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
-			maximumString = num1;
+			maximum = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
-			maximumString = num2;
+			maximum = num2;
 		else
-			maximumString = num3;
-		return maximumString;
+			maximum = num3;
+		return maximum;
 
 	}
 	
-	//print maximum String
+	//print maximum of three methods
 	public static void main(String[] args) {
-		System.out.println("Maximum String Among 3 will be :- " + maximumString("Papaya", "Apple", "Mango"));
-	}
+		System.out.println("The max Number Among Three String numbers is:-\n "+maximumAmongThree("Papaya", "Apple", "Mango"));
+		System.out.println("The max Number Among Three Integer numbers is:-\n "+maximumAmongThree(70, 40, 50));
+		System.out.println("The max Number Among Three Float numbers is:-\n "+maximumAmongThree(70.6f, 40.7f, 50.8f));	}
 
 }
