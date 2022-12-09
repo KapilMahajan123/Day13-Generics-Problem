9b7f0f93414fd8569121e5b80e21aef46bb38196
kapilmahajan123 <montyempire123gmail.com>
2022-12-09 12:14:41 +0530

    Added: More than three Parameter

--- src/com/Generics/java/MaximumTestGenerics.java
+++ src/com/Generics/java/MaximumTestGenerics.java
@@ -10,33 +10,49 @@ public class MaximumTestGenerics<T>{
 	/**
 	 * here i have used Generic functions.
 	 * here T is the type of data.
-	 * Gradually it will return the maximum.
 	 */
-	T num1, num2, num3;
-	public MaximumTestGenerics(T num1, T num2, T num3)
+	T num1, num2, num3, num4;
+	
+	/*
+	 * Here this one is parameterised constructor having these parameters to initiate the values.
+	 * @param num1
+	 * @param num2
+	 * @param num3
+	 * @param num4
+	 */
+	public MaximumTestGenerics(T num1, T num2, T num3, T num4)
 	{
 		this.num1 = num1;
 		this.num2 = num2;
 		this.num3 = num3;
+		this.num4 = num4;
+
 	}
-	
-	public static <T extends Comparable<T>> T maximumAmongThree(T num1, T num2, T num3) {
+	/*
+	 * In this method it will check the values
+	 * And give the maximum as an output.
+	 */
+	public static <T extends Comparable<T>> T maximumAmongFour(T num1, T num2, T num3, T num4) {
 
 		T maximum = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
 			maximum = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
 			maximum = num2;
-		else
+		else if (num3.compareTo(num4) > 0)
 			maximum = num3;
-		return maximum;
+		else
+			maximum = num4;
+		return maximum;		 //Gradually it will return the maximum.
+
 
 	}
 	
-	//print maximum of three methods
+	//print
 	public static void main(String[] args) {
-		System.out.println("The max Number Among Three String numbers is:-\n "+maximumAmongThree("Orange", "Dragonfruit", "Apple"));
-		System.out.println("The max Number Among Three Integer numbers is:-\n "+maximumAmongThree(14, 56, 27));
-		System.out.println("The max Number Among Three Float numbers is:-\n "+maximumAmongThree(39.6f, 78.7f, 99.8f));	}
+		System.out.println("The max Number Among Four String numbers is:-\n "+maximumAmongFour("Orange", "Dragonfruit", "Apple", "Mango"));
+		System.out.println("The max Number Among Four Integer numbers is:-\n "+maximumAmongFour(14, 56, 27, 40));
+		System.out.println("The max Number Among Four Float numbers is:-\n "+maximumAmongFour(39.6f, 78.7f, 99.8f, 53.1f));	
+		}
 
 }
